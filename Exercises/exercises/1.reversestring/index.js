711a87d1faf2af9f2a475b9068f527c947b29316
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2021-01-01 13:41:59 +0800

    Update index.js

--- Exercises/exercises/1.reversestring/index.js
+++ Exercises/exercises/1.reversestring/index.js
@@ -6,22 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+//!Solution 1
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+//!solution 2
+function reverse(str) {
+  let reversed = "";
+  for (let i of str) {
+    reversed = i + reversed;
+  }
+  return reversed;
+}
+
+//!solution 3
 function reverse(str) {
   return str.split("").reduce((rev, char) => char + rev, "");
 }
-
-//!Solution 1
-// function reverse(str) {
-//   return str.split("").reverse().join("");
-// }
-
-//!solution 2
-// function reverse(str) {
-//     let reversed = "";
-//     for (let character of str) {
-//       reversed = character + reversed;
-//     }
-//     return reversed;
-//   }
 
 module.exports = reverse;
