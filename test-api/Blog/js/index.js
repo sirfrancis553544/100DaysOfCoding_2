36f1b105f4483c3d5def71cfa9857b11ee1948fc
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2021-02-24 09:11:19 +0800

    details page

---
+++ test-api/Blog/js/index.js
@@ -0,0 +1,22 @@
+const container = document.querySelector(".blogs");
+
+const renderPosts = async () => {
+  let uri = " http://localhost:3000/posts?_sort";
+  const res = await fetch(uri);
+  const posts = await res.json();
+  console.log(posts);
+  let template = "";
+
+  posts.forEach((post) => {
+    template += `
+      <div class="post">
+      <h2>${post.title}</h2>
+      <p><small>${post.likes} likes</small></p>
+      <p>${post.body.slice(0, 200)}</p>
+      <a href="details.html?id=${post.id}"> read more...</a>
+      </div>`;
+  });
+  container.innerHTML = template;
+};
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
