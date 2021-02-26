4fc6bd090b92ddc5b8b855c01cf14c19b8cbbf55
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2021-02-26 21:57:20 +0800

    delete

--- test-api/Blog/js/index.js
+++ test-api/Blog/js/index.js
@@ -1,7 +1,12 @@
 const container = document.querySelector(".blogs");
+const searchForm = document.querySelector(".search");
 
-const renderPosts = async () => {
+const renderPosts = async (term) => {
   let uri = "http://localhost:3000/posts?_sort=likes&_order=desc";
+  if (term) {
+    uri += `&q=${term}`;
+  }
+
   const res = await fetch(uri);
   const posts = await res.json();
   console.log(posts);
@@ -18,5 +23,10 @@ const renderPosts = async () => {
   });
   container.innerHTML = template;
 };
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  renderPosts(searchForm.term.value.trim());
+});
 
 window.addEventListener("DOMContentLoaded", () => renderPosts());
